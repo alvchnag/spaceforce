c469164d5048219880b15b28c206b1db90c832e4
Alvin Chang <alvchang@ucdavis.edu>
2018-08-19 13:05:45 -0700

    Fixed movement

--- spaceforce.js
+++ spaceforce.js
@@ -6,29 +6,68 @@ var ship = new Image();
 ship.src = "/Users/alvin/Documents/Projects/spaceforce/img/blueship1.png";
 var soundtrack = new Audio("/Users/alvin/Documents/Projects/spaceforce/audio/soundtrack.mp3");
 soundtrack.play();
+soundtrack.loop = true;
+document.addEventListener("keydown",onkeydown,false);
+document.addEventListener("keyup",onkeyup,false);
+var keystate = {};
+stars = [];
 
-for(i=0; i<100; i++){
-	ctx.arc(Math.floor(Math.random()* canvas.width),Math.floor(Math.random()* canvas.height),10,0,2*Math.PI);
-	ctx.fillStyle = "white";
-    ctx.beginPath();
-    ctx.fill();
+function star(){
+	this.x = Math.floor(Math.random()* canvas.width);
+	this.y = Math.floor(Math.random()* canvas.height);
+}
+
+for(i=0; i<500; i++){
+	var s = new star();
+	stars.push(s);
 }
 
 var player = {
-	x: canvas.width/2-ship.width/2,
-	y: canvas.height/2-ship.height/2, 
+	x: canvas.width/2+ship.width/2,
+	y: canvas.height/2+ship.height/2,
+	vx: 0,
+	vy: 0,
 	health: 100,
 }
 
 function game(){
-	ctx.drawImage(ship,player.x,player.y);
-	window.addEventListener("keydown",onkeydown,false);
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+	checkkeys();
+	ctx.drawImage(ship,player.x += player.vx, player.y += player.vy);
+	for(i=0; i<500; i++){
+		ctx.beginPath();
+		ctx.arc(stars[i].x,stars[i].y,1,0,2*Math.PI);
+		ctx.fillStyle = "#FFFFFF";
+    	ctx.fill();
+	}
 	requestAnimationFrame(game);
 }
 
 function onkeydown(key){
-	if(key == 37){
-		player.x -= 10;
+	if(key.keyCode == "37"){
+		keystate[key.keyCode] = true;
+	}
+	if(key.keyCode == "39"){
+		keystate[key.keyCode] = true;
+	}
+}
+
+function onkeyup(key){
+	for(key in keystate){
+		keystate[key] = false;
+	}
+}
+
+function checkkeys(){
+	if(keystate["37"] && player.x > 0){
+		player.vx = -15;
+	}
+	else if(keystate["39"] && player.x + ship.width < canvas.width){
+		player.vx = 15;
+	}
+	else{
+		player.vx = 0;
 	}
 }
 
