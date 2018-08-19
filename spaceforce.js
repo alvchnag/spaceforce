c70447a7ee100d402a5c61ab132e21db7cbeab82
Alvin Chang <alvchang@ucdavis.edu>
2018-08-18 17:55:37 -0700

    Added audio file

--- spaceforce.js
+++ spaceforce.js
@@ -7,9 +7,29 @@ ship.src = "/Users/alvin/Documents/Projects/spaceforce/img/blueship1.png";
 var soundtrack = new Audio("/Users/alvin/Documents/Projects/spaceforce/audio/soundtrack.mp3");
 soundtrack.play();
 
+for(i=0; i<100; i++){
+	ctx.arc(Math.floor(Math.random()* canvas.width),Math.floor(Math.random()* canvas.height),10,0,2*Math.PI);
+	ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.fill();
+}
+
+var player = {
+	x: canvas.width/2-ship.width/2,
+	y: canvas.height/2-ship.height/2, 
+	health: 100,
+}
+
 function game(){
-	ctx.drawImage(ship,canvas.width/2-ship.width/2,canvas.height/2-ship.height/2);
+	ctx.drawImage(ship,player.x,player.y);
+	window.addEventListener("keydown",onkeydown,false);
 	requestAnimationFrame(game);
+}
+
+function onkeydown(key){
+	if(key == 37){
+		player.x -= 10;
+	}
 }
 
 game();
